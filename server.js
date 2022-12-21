8147bf7194f3786a51dad1406c637dcd3ec4d2e3
kruamas <kruamad.ak@rmuti.ac.th>
2022-12-21 21:48:53 +0700

    add /api

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("",require("./src/routes/index_routes"))
+app.use("/api",require("./src/routes/index_routes"))
 
 const distPath = path.join(__dirname, '/build');
 app.use(express.static(distPath));
